19d063120ce8a257636e78e9e69df1ef15014c92
Ananthapadmanabhan.s.s <ananthasayanam2008@gmail.com>
2024-03-13 14:58:01 +0530

    PATTERN 1

---
+++ lesson1/pattern.java
@@ -0,0 +1,13 @@
+package lesson1;
+
+public class pattern {
+    public static void main(String[] args) {
+        //Since we are starting from basics we shall not use loops
+        //we are gonna print the pattern using basic print function
+        System.out.println("****"); 
+        System.out.println("***");
+        System.out.println("**");
+        System.out.println("*");
+    }
+    
+}
