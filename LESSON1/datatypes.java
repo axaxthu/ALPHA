5e9dea86952a5be9f54cb25d6118550598f512ee
Ananthapadmanabhan.s.s <ananthasayanam2008@gmail.com>
2024-03-15 12:26:29 +0530

    DATA TYPES IN JAVA

---
+++ LESSON1/datatypes.java
@@ -0,0 +1,12 @@
+package LESSON1;
+//different datatypes in java
+public class datatypes {
+    public static void main(String[] args) {
+        int a=0;
+        float b=1.5f;
+        char c='a';
+        double x=67.0;
+        //other data types are short,byte,long,String
+        System.out.println(a+" "+b+" "+c+" "+x);
+    }
+}
