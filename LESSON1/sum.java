1a0ec4801cda19d85925e4ce5460ce1b9d9b5bcd
Ananthapadmanabhan.s.s <ananthasayanam2008@gmail.com>
2024-03-15 12:28:35 +0530

    SUM OF TWO NUMBERS

---
+++ LESSON1/sum.java
@@ -0,0 +1,9 @@
+package LESSON1;
+//sum of two numbers
+public class sum {
+    public static void main(String[] args) {
+        int a=0,b=10;
+        int sum=a+b;
+        System.out.println(sum);
+    }
+}
