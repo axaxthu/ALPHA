fe3cb157c491c87482838b32720055d386feb1e2
Ananthapadmanabhan.s.s <ananthasayanam2008@gmail.com>
2024-03-18 23:51:54 +0530

    UNARY OPERATORS

---
+++ LESSON2/unaryop.java
@@ -0,0 +1,11 @@
+package LESSON2;
+
+public class unaryop {
+    public static void main(String[] args) {
+        //UNARY OPERATORS
+        int a=10,z=10;
+        int b=++a;
+        int y=z++;
+        System.out.println(b+" "+y);
+    }
+}
