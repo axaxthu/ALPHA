1f6a68530b57790b03fe1f8bef7b5ec40023bf2e
Ananthapadmanabhan.s.s <ananthasayanam2008@gmail.com>
2024-03-18 23:44:23 +0530

    ARITHMETIC OPERATORS

---
+++ LESSON2/arthop.java
@@ -0,0 +1,16 @@
+package LESSON2;
+
+public class arthop {
+    public static void main(String[] args) {
+        //ARITHMETIC OPERATORS
+        int a=19;
+        int b=2;
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+        System.out.println(a%b);
+
+
+    }
+}
