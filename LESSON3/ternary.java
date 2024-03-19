783e0951bddbf7befd0e88b37c4304807507ce85
Ananthapadmanabhan.s.s <ananthasayanam2008@gmail.com>
2024-03-19 20:33:05 +0530

    TERNARY OPERATORS

---
+++ LESSON3/ternary.java
@@ -0,0 +1,10 @@
+package LESSON3;
+
+public class ternary {
+  public static void main(String[] args) {
+    //checking odd or even using ternary operator
+    int d=4;
+    String type=((d%2)==0)?"even":"odd";
+    System.out.println(type);
+  }   
+}
