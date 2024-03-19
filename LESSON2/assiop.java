9cfb3ce89adf2172a0f54e04ff5c820bb0d2b655
Ananthapadmanabhan.s.s <ananthasayanam2008@gmail.com>
2024-03-19 09:45:40 +0530

    ASSIGNMENT OPERATORS

---
+++ LESSON2/assiop.java
@@ -0,0 +1,13 @@
+package LESSON2;
+
+public class assiop {
+    public static void main(String[] args) {
+        int a=10,b=3;
+        int c=a;
+        a+=b;
+        c-=b;
+        b*=a;
+        c/=2;
+        System.out.println(a+" "+b+" "+c);
+    }
+}
