959ecc29a2ad8b400ab47e28bfb772a8ff3503f6
Ananthapadmanabhan.s.s <ananthasayanam2008@gmail.com>
2024-03-19 20:27:52 +0530

    LARGER OF 2 NUMBERS

---
+++ LESSON3/largerof2.java
@@ -0,0 +1,14 @@
+package LESSON3;
+
+public class largerof2 {
+    public static void main(String[] args) {
+        int a=12,b=56;
+        if(a>=b){
+            System.out.println(a);
+        }
+        else{
+            System.out.println(b);
+        }
+    }
+}
+//printing the larger number
