e3b7ba32838c61986a8e4c6ffd15de1678e6a3e0
Ananthapadmanabhan.s.s <ananthasayanam2008@gmail.com>
2024-03-19 09:41:10 +0530

    LOGICAL OPERATORS

---
+++ LESSON2/logop.java
@@ -0,0 +1,13 @@
+package LESSON2;
+
+public class logop {
+    public static void main(String[] args) {
+        //int a=10;
+        //int b=1;
+        //System.out.println(a&&b); this is an error statement in java(you cant compare numbers using logical operators)
+        System.out.println((3>2)&&(4>3));
+        System.out.println((4>2)||(5>7));
+        System.out.println(!(3>1));
+    }
+    
+}
