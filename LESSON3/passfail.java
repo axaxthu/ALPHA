b3030a14223cac35f6a89ddef0075ed3fa960230
Ananthapadmanabhan.s.s <ananthasayanam2008@gmail.com>
2024-03-19 20:34:09 +0530

    PASS OR FAIL

---
+++ LESSON3/passfail.java
@@ -0,0 +1,10 @@
+package LESSON3;
+//import java.util.*;
+public class passfail {
+    public static void main(String[] args) {
+        
+        int marks=67;
+        String report=marks>33?"pass":"FAIL";
+        System.out.println(report);
+    }
+}
