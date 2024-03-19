eb670043cd8ed97a1a6f0ce4174f25921a639098
Ananthapadmanabhan.s.s <ananthasayanam2008@gmail.com>
2024-03-19 09:32:51 +0530

    RELATIONAL OPERATORS

---
+++ LESSON2/relop.java
@@ -0,0 +1,14 @@
+package LESSON2;
+
+public class relop {
+    public static void main(String[] args) {
+        int a=10;
+        int b=5;
+        System.out.println(a==b); //false
+        System.out.println(a!=b); //true
+        System.out.println(a>b); //true
+        System.out.println(a<b); //false
+        System.out.println(a>=b); //true
+        System.out.println(a<=b); //false
+    }
+}
