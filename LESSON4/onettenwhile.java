e9aca5c9e67e4fdab521278851a55c675f25ebc8
Ananthapadmanabhan.s.s <ananthasayanam2008@gmail.com>
2024-03-20 21:03:33 +0530

    PRINT FROM 1 TO 10 USING WHILE LOOP

---
+++ LESSON4/onettenwhile.java
@@ -0,0 +1,11 @@
+package LESSON4;
+//PRINT NUMBERS FROM ONE TO TEN USING WHILE
+public class onettenwhile {
+    public static void main(String[] args) {
+        int c=1;
+        while(c<=10){
+            System.out.println(c);
+            c++;
+        }
+    }
+}
