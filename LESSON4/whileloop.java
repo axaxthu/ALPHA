a9cef73fdbeb24addacd71336549370d1342f68a
Ananthapadmanabhan.s.s <ananthasayanam2008@gmail.com>
2024-03-20 21:01:53 +0530

    WHILE LOOP

---
+++ LESSON4/whileloop.java
@@ -0,0 +1,11 @@
+package LESSON4;
+
+public class whileloop {
+    public static void main(String[] args) {
+         int c=0;
+        while(c<100){
+            System.out.println("HELLO WORLD");
+            c++;
+        } 
+    }
+}
