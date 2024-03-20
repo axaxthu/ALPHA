f1a835a974f25a6cb725f97db9ad897e4832563d
Ananthapadmanabhan.s.s <ananthasayanam2008@gmail.com>
2024-03-20 23:11:24 +0530

    SQUARE PATTERN BASIC

---
+++ LESSON4/sqrpattern.java
@@ -0,0 +1,17 @@
+package LESSON4;
+
+public class sqrpattern {
+    public static void main(String[] args) {
+        /*
+        $$$$
+        $$$$
+        $$$$
+        $$$$
+         */
+        for(int i=1;i<=4;i++){
+            System.err.println("$$$$");
+        }
+
+        
+    }
+}
