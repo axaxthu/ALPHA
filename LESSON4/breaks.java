3ca0d4a668bc6a4fcdc061f82766bfe01a389ec5
Ananthapadmanabhan.s.s <ananthasayanam2008@gmail.com>
2024-03-26 17:32:18 +0530

    BREAK STATEMENT

---
+++ LESSON4/breaks.java
@@ -0,0 +1,13 @@
+package LESSON4;
+
+public class breaks {
+    public static void main(String[] args) {
+        for(int i=1;i<=5;i++){
+            if(i==3){
+                break;
+            }
+            System.out.println(i);
+        }
+    }
+}
+//program that stops printing if i is 3
