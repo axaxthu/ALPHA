60306263026bd08b76ac956ee9b9e60d373f4b15
Ananthapadmanabhan.s.s <ananthasayanam2008@gmail.com>
2024-03-26 17:36:52 +0530

    PRIME NUMBER USING LOOPS

---
+++ LESSON4/prime.java
@@ -0,0 +1,26 @@
+package LESSON4;
+import java.util.*;
+public class prime {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        if(n==2){
+            System.out.println("PRIME NO");
+        }
+        else{
+        boolean isprime=true;
+        for(int i=2;i<=Math.sqrt(n);i++){ //math.sqrt reduces time complexity
+            if(n%i==0){
+                isprime=false;
+            }
+        }
+        if(isprime==true){
+            System.out.println("PRIME NO!!!");
+        }
+        else{
+            System.out.println("not PRIME NO!!!");
+        }
+    }
+    sc.close();
+}
+}
