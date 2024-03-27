2670d07b2bbad2ba023b0878d4a9de458ecd7c75
Ananthapadmanabhan.s.s <ananthasayanam2008@gmail.com>
2024-03-27 11:46:53 +0530

    STAR PATTERN

---
+++ LESSON5/star1.java
@@ -0,0 +1,19 @@
+package LESSON5;
+/*
+ *
+ **
+ ***
+ ****
+ PRINT THIS PATTERN
+ */
+
+public class star1 {
+    public static void main(String[] args) {
+        for(int i=1;i<=4;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.err.println();
+        }
+    }
+}
