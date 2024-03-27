f8a68cb8da6a511eb64f784f1d2528e75ef0706c
Ananthapadmanabhan.s.s <ananthasayanam2008@gmail.com>
2024-03-27 11:49:20 +0530

    CHARACTER PATTERN

---
+++ LESSON5/charpattern.java
@@ -0,0 +1,13 @@
+package LESSON5;
+
+public class charpattern {
+    public static void main(String[] args) {
+        char ch='A';
+        for(int i=1;i<=4;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(ch++);
+            }
+            System.out.println();
+        }
+    }
+}
