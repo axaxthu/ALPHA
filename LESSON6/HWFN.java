ceb107387d036d7e161baf2c6a990b095ac9980b
Ananthapadmanabhan.s.s <ananthasayanam2008@gmail.com>
2024-04-03 11:37:47 +0530

    HELLO WORLD USING FUNCTIONS

---
+++ LESSON6/HWFN.java
@@ -0,0 +1,10 @@
+package LESSON6;
+//PRINT HELLO WORLD USING FUNCTIONS
+public class HWFN {
+    public static void printhelloworld(){
+        System.out.println("HELLO WORLD");
+    }
+    public static void main(String[] args) {
+        printhelloworld();;
+    }
+}
