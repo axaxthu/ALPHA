890b9a81fcd0a3ccfdefe36a4038fac903533991
Ananthapadmanabhan.s.s <ananthasayanam2008@gmail.com>
2024-04-03 11:41:34 +0530

    PRODUCT USING FUNCTIONS

---
+++ LESSON6/prod.java
@@ -0,0 +1,13 @@
+package LESSON6;
+
+public class prod {
+    public static int multi(int a, int b) {
+        int pro=a*b;
+        return pro;
+    }
+    public static void main(String[] args) {
+        int a=50,b=3;
+        int pro=multi(a, b);
+        System.out.println(pro);
+    }
+}
